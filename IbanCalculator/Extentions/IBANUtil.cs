b0589526202fa4c0beb1f9df41d47afdf4cd0430
Schneider David <Schneider David@DESKTOP-NHBJNJ5>
2023-05-23 12:05:36 +0200

    Projektdateien hinzufügen.

---
+++ IbanCalculator/Extentions/IBANUtil.cs
@@ -0,0 +1,45 @@
+﻿namespace IbanValidator.Extentions;
+
+internal static class IBANUtil
+{
+    internal static bool ValidateIBAN(string iban, decimal pz)
+    {
+        return pz == GetValidator(iban);
+    }
+
+    internal static decimal GetValidator(string iban)
+    {
+        decimal countryCode = iban.GetCountryCode();
+        decimal accountNr = iban.GetAccountNr();
+
+        string calcNrText = accountNr.ToString() + countryCode.ToString() + "00";
+        decimal calcNr = decimal.Parse(calcNrText);
+
+        decimal remainder = calcNr % 97;
+        decimal validator = 98 - remainder;
+
+        return validator;
+    }
+
+    internal static int GetCountryCode(this string iban)
+    {
+        string value = iban.Substring(0, 2);
+        string countryCodeText = value[0].ToLetterCode().ToString() + value[1].ToLetterCode().ToString();
+        int countryCode = Convert.ToInt32(countryCodeText);
+        return countryCode;
+    }
+
+    internal static decimal ToLetterCode(this char letter) => (int)letter - 55;
+
+    internal static decimal GetAccountNr(this string iban)
+    {
+        string value = iban.Substring(4, iban.Length - 4);
+        decimal accountNr = decimal.Parse(value);
+        return accountNr;
+    }
+
+    internal static decimal GetPZ(this string iban)
+    {
+        return Convert.ToDecimal(iban.Substring(2, 2));
+    }
+}
---
+++ IbanCalculator/Program.cs
@@ -0,0 +1,63 @@
+﻿using IbanValidator.Extentions;
+using SinKien.IBAN4Net;
+using Spectre.Console;
+using System.Text.RegularExpressions;
+
+namespace IbanValidator;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            string iban = AskForIBAN();
+            bool validIBAN = IBANUtil.ValidateIBAN(iban, iban.GetPZ());
+            decimal pzIBAN = IBANUtil.GetValidator(iban);
+
+            ShowIBANResult(validIBAN, pzIBAN);
+        }
+    }
+
+    static string AskForIBAN()
+    {
+        string iban = string.Empty;
+        bool isValidIBAN = false;
+        do
+        {
+            iban = AnsiConsole.Ask<string>("Enter your [aqua]IBAN[/]:");
+            isValidIBAN = Regex.Match(iban, @"\w\w\d+$").Success;
+            if (!isValidIBAN)
+            {
+                AnsiConsole.MarkupLine("\n[red]Invalid IBAN format![/]");
+                PressKeyToContinue();
+            }
+        }
+        while (!isValidIBAN);
+
+        return iban;
+    }
+
+    static void ShowIBANResult(bool isValidIBAN, decimal pz)
+    {
+        AnsiConsole.Clear();
+
+        if (isValidIBAN)
+            AnsiConsole.MarkupLine("Your IBAN is [aqua]valid[/]!");
+        else
+            AnsiConsole.MarkupLine("Your IBAN is [red]valid[/]!");
+
+        AnsiConsole.MarkupLine($"Validaton number is: [aqua]{pz}[/]");
+
+        AnsiConsole.WriteLine();
+
+        PressKeyToContinue();
+    }
+
+    static void PressKeyToContinue()
+    {
+        AnsiConsole.MarkupLine("[grey]Press any key to continue...[/]");
+        Console.ReadKey();
+        AnsiConsole.Clear();
+    }
+}
